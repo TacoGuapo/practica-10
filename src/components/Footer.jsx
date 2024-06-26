3a7db08fa63da85aee9d5627569cbfcaa3e93bf8
Francis <karma.police190@gmail.com>
2024-06-26 16:14:35 -0500

    practica10

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export function Footer () {
+
+  return (
+    <footer className='footer'>
+      <h4>Prueba técnica de React ⚛️ － <span>@TacoGuapo</span></h4>
+      <h5>Practica 10 React</h5>
+    </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Filters } from './Filters.jsx'
+
+export function Header () {
+  return (
+    <header>
+      <h1>Practica 10 🛒</h1>
+      <Filters />
+    </header>
+  )
+}
